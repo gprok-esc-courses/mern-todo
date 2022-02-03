6de62d3071f87bd5a16988c6a4a589d70b4f9aa2
gprok <gprok60@gmail.com>
2022-02-03 19:54:48 +0200

    todo comments for tasks of current user

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -4,6 +4,11 @@ const Task = require("../models/task.model")
 
 router.get("/", async (req, res) => {
     try {
+        // ### TODO
+        // check if there is a token
+        // decode token 
+        // get user id (as uid)
+        // { user_id : { $eq uid }}
         const tasks = await Task.find({})
         res.json(tasks)
     }
@@ -13,11 +18,15 @@ router.get("/", async (req, res) => {
 })
 
 router.post("/add", async (req, res) => {
+    // ### TODO
+    // find token, if any and decode
+    // get user id form token as uid
     try {
         let {name} = req.body 
         let newTask = new Task({
             name: name,
             completed: false,
+            user_id: uid,
         })
         const saved = await newTask.save()
         res.json(saved)
@@ -26,5 +35,7 @@ router.post("/add", async (req, res) => {
         return res.status(500).json({error: err.message})
     }
 })
+
+// tasks/complete req -> task id and make completed true
 
 module.exports = router
